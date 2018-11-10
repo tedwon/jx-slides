c2c6dfad72e97d5dc7f5cc1cd728c37f2c6ec2c5
tedwon <jwon@redhat.com>
2018-11-11 01:23:19 +0900

    Jenkins X Introduction Slides

---
+++ src/main/java/org/jbugkorea/slides/SlidesApplication.java
@@ -0,0 +1,12 @@
+package org.jbugkorea.slides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlidesApplication.class, args);
+	}
+}
